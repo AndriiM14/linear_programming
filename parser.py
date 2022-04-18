0cdf206a2bb34bb92e4fa4e756e0d57b3ce59176
andriikjr <andrijmarusik@gmail.com>
2022-04-18 15:43:45 +0300

    commented parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+"""
+Module for parsing models from input files
+"""
 from model import Model, Expression
 from pulp import LpMinimize, LpMaximize
 from typing import Callable
@@ -5,14 +8,25 @@ from consts import ParsingConsts, ParsingTypes
 
 
 def parse_model(path: str) -> Model:
+    """
+    Parses file and returns instance of Model
+    :param path: the path to the input file
+    :return: returns Model parsed from the file
+    """
     model = Model()
 
     with open(path, "r") as md_data:
         problem_tag = md_data.readline()
 
+        """
+        Checking if provided file describes problem
+        """
         if problem_tag.find(ParsingTypes.PROBLEM_TAG.value) == -1:
             raise Exception("Wrong file. Model files should start with \"Problem:\" tag")
 
+        """
+        Going through lines of the file and parsing data for the Model
+        """
         for line in md_data:
             if line.find(ParsingTypes.SENSE.value) != -1:
                 model.sense = parse_sense(line)
@@ -25,6 +39,11 @@ def parse_model(path: str) -> Model:
 
 
 def parse_sense(line: str) -> LpMinimize | LpMaximize:
+    """
+    Translate MIX | MAX into LpMinimize | LpMaximize
+    :param line: line from the fie with "Sense:" tag
+    :return: returns LpMinimize or LpMaximize const from pulp library
+    """
     return ParsingConsts.SenseTranslator[
         line.strip()
             .removeprefix(ParsingTypes.SENSE.value)
@@ -34,16 +53,31 @@ def parse_sense(line: str) -> LpMinimize | LpMaximize:
 
 
 def parse_obj_fun(line: str) -> Expression:
+    """
+    Parses expression for the objective function
+    :param line: line from the file with "Fun:" tag
+    :return: returns Expression for the objective function
+    """
     expr = parse_expression(line.strip().removeprefix(ParsingTypes.OBJ_FUN.value))
     return expr
 
 
 def parse_constraint(line: str) -> Expression:
+    """
+    Parses expression for the constraint
+    :param line: line from the file with "Constraint:" tag
+    :return: returns Expression for the constraint
+    """
     expr = parse_expression(line.strip().removeprefix(ParsingTypes.CONSTRAINT.value))
     return expr
 
 
 def parse_expression(line: str) -> Expression:
+    """
+    Tokenizes line, extract values, build Expression
+    :param line: line to tokenize and get the value from
+    :return: returns built Expression
+    """
     expr = Expression()
 
     tokens = line.split(";")
@@ -62,5 +96,13 @@ def parse_expression(line: str) -> Expression:
 
 
 def extract_value(token: str, prefix: str = "", suffix: str = "", converter: Callable = str):
+    """
+    Extracts value from the token and converts to the needed type
+    :param token: Token where value will be extracted
+    :param prefix: Prefix of the token to remove
+    :param suffix: Suffix of the token to remove
+    :param converter: Callable object that converts value of type string to needed type
+    :return: returns a value of type that converter converts to
+    """
     value = token.strip().removeprefix(prefix).removesuffix(suffix).strip()
     return converter(value)
